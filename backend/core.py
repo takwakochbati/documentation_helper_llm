45e9ec398d1a48643f00ec7898772b24821a3c04
Takwa Kochbati <kochbatitakwa1@gmail.com>
2024-11-09 12:04:45 +0100

    create a RAG (QA chat) from LangChain documentation using Pinecone & llama3/OpenAI

---
+++ backend/core.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from langchain.chains.retrieval import create_retrieval_chain
+
+load_dotenv()
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_pinecone import PineconeVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_ollama import ChatOllama
+
+INDEX_NAME="langchain-doc-index"
+
+def run_llm(query: str):
+    embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large")
+    docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
+    llm = ChatOllama(model = "llama3.1")
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    stuff_documents_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    qa = create_retrieval_chain(retriever=docsearch.as_retriever(),combine_docs_chain=stuff_documents_chain)
+    result = qa.invoke(input={"input":query})
+    return result
+
+if __name__ == "__main__":
+    res = run_llm(query="what is LangChain chain?")
+    print(res["answer"])
