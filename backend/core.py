5c480c60952c13ad76a2a1588ee168342f137908
Takwa Kochbati <kochbatitakwa1@gmail.com>
2024-11-10 14:26:31 +0100

    add readme.md

--- backend/core.py
+++ backend/core.py
@@ -19,8 +19,13 @@ def run_llm(query: str):
     stuff_documents_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
     qa = create_retrieval_chain(retriever=docsearch.as_retriever(),combine_docs_chain=stuff_documents_chain)
     result = qa.invoke(input={"input":query})
-    return result
+    new_result = {
+         "query": result["input"],
+         "result": result["answer"],
+         "source_documents": result["context"],
+    }
+    return new_result
 
 if __name__ == "__main__":
     res = run_llm(query="what is LangChain chain?")
-    print(res["answer"])
+    print(res["result"])
