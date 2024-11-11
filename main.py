a0bddce10685efcc305d9af0cca8ae6db15f27b1
Takwa Kochbati <kochbatitakwa1@gmail.com>
2024-11-11 17:56:16 +0100

    add picture readme

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def create_sources_string(source_urls: Set[str]) -> str:
     sources_list.sort()
     sources_string = "sources:\n"
     for i, source in enumerate(sources_list):
-        sources_string += f"{i+1}. {source}\n"
+        sources_string += f"{source}\n"
     return sources_string
 
 
